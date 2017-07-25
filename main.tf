9771b487d311e855f8d1786c77b1282ea23b1fcd
sinergiabinaria <git@sinergiabinaria.com>
2017-07-25 15:35:28 -0500

    Adding Security Monitoring Software and Dependencies

--- main.tf
+++ main.tf
@@ -229,6 +229,39 @@ resource "aws_instance" "web" {
     }
   }
 
+  #Security Monitoring Software and Dependencies 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get install nagios-nrpe-server libnagios-plugin-perl nagios-plugins libsys-statistics-linux-perl -y",
+      "sudo sed -i -e 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1,192.99.99.97/' /etc/nagios/nrpe.cfg",
+      "sudo echo "command[check_users]=/usr/lib64/nagios/plugins/check_users -w 1 -c 2",
+      "command[check_load]=/usr/lib64/nagios/plugins/check_load -w 5,5,5 -c 10,10,10",
+      "command[check_hdhome]=/usr/lib64/nagios/plugins/check_disk -w 40% -c 30%",
+      "command[check_procs]=/usr/lib64/nagios/plugins/check_procs -w 150 -c 200",
+      "command[check_uptime]=/usr/lib64/nagios/plugins/check_uptime" >> /etc/nagios/nrpe.cfg",
+      "sudo /etc/init.d/nagios-nrpe-server start",
+      "sudo update-rc.d nagios-nrpe-server defaults",
+      "sudo apt install snmpd -y",
+      "sudo sed -i -e 's/agentAddress  udp:127.0.0.1:161/agentAddress  udp:161/' /etc/snmp/snmpd.conf",
+      "sudo sed -i -e 's/#rocommunity public  localhost/rocommunity public 149.56.84.50/' /etc/snmp/snmpd.conf",
+      "sudo sed -i -e 's/sysLocation    Sitting on the Dock of the Bay/sysLocation    Guadalajara, Jal, Mex./' /etc/snmp/snmpd.conf",
+      "sudo sed -i -e 's/sysContact     Me <me@example.org>/sysContact     operations@sinergia.io/' /etc/snmp/snmpd.conf",
+      "sudo iptables -I INPUT -p tcp -s 149.56.84.50 -j ACCEPT",
+      "sudo iptables -I OUTPUT -p tcp -d  149.56.84.50 -j ACCEPT",
+      "sudo iptables -I INPUT -p udp -m udp --dport 161 -j ACCEPT",
+      "sudo iptables -I INPUT -p udp -m udp --dport 162 -j ACCEPT",
+      "sudo iptables-save ",
+      "sudo service snmpd restart"
+    ]
+
+    connection {
+      port        = "12345"
+      timeout     = "1m"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+  }
+  
+
   provisioner "remote-exec" {
     inline = [
       "mkdir -p /home/ubuntu/apps/tdc/source",
